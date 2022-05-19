bd566e496eb7babc1b11f814ed1b895f90064d79
Khalilou Konate <KhalilKo@1stSourceCorp.com>
2022-05-19 16:48:46 -0400

    ok

---
+++ src/books.js
@@ -0,0 +1,34 @@
+
+
+
+export const pook=[
+    { id:1,
+        titlee:'It ENDS WITH US',
+        authorr:'Amelia',
+        imaag:'https://images-na.ssl-images-amazon.com/images/I/71EwoNQypZL._AC_UL210_SR195,210_.jpg'
+    },
+    {id:2,
+        titlee:'Half Baked',
+        authorr:'Konate',
+        imaag:'https://images-na.ssl-images-amazon.com/images/I/51xiuHJv5DL._SX398_BO1,204,203,200_.jpg'
+    },
+    {id:3,
+        titlee:'Atlas of the Heart: Mapping Meaningful Connection and the Language of Human Experience',
+        authorr:'Khalilou',
+        imaag:'https://m.media-amazon.com/images/I/81MgglteUFL._AC_UY218_.jpg'
+    }
+];
+
+const firstB={
+    title:'It ENDS WITH US',
+    author:'Amelia',
+    imag:'https://images-na.ssl-images-amazon.com/images/I/71EwoNQypZL._AC_UL210_SR195,210_.jpg'
+}
+
+
+const SecondB={
+    title:'Half Baked',
+    author:'Konate',
+    imag:'https://images-na.ssl-images-amazon.com/images/I/51xiuHJv5DL._SX398_BO1,204,203,200_.jpg'
+}
+
--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom';
+import  './index.css';
+import Boo from './Book'
+import {pook} from './books'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+function BookList() {
+   
+    return (<section className='booklist' >
+          
+       {pook.map((y)=> {
+                        return <Boo key={y.id} {...y}></Boo>})}
+        </section>
+    )}
+
+
+
+ReactDOM.render(<BookList />, document.getElementById('root'));
+
