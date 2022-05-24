2d9ad98fb8992d5fe214094c593a9f3ae083e589
Khalilou Konate <KhalilKo@1stSourceCorp.com>
2022-05-24 15:55:04 -0400

    ok

--- src/books.js
+++ src/books.js
@@ -10,7 +10,7 @@ export const pook=[
     {id:2,
         titlee:'Half Baked',
         authorr:'Konate',
-        imaag:'https://images-na.ssl-images-amazon.com/images/I/51xiuHJv5DL._SX398_BO1,204,203,200_.jpg'
+        imaag:"https://media-exp1.licdn.com/dms/image/C4E03AQGOQhORdwKZag/profile-displayphoto-shrink_400_400/0/1611567868269?e=1658361600&v=beta&t=LyhcI4jmXB5bRTh66OeiEkdp7UKfDTeo11f-DoDUOio"
     },
     {id:3,
         titlee:'Atlas of the Heart: Mapping Meaningful Connection and the Language of Human Experience',
